5aa92b22eccd0f9f16b0dbe19d9f36c322b68696
Bogdan Kuzyk <bogdankuzyk14@gmail.com>
2023-12-14 11:07:52 +0200

    Create a Form in ClientsForm.jsx

--- src/components/ClientForm.jsx
+++ src/components/ClientForm.jsx
@@ -1,5 +1,41 @@
 const ClientForm = () => {
-  return <div></div>;
+  return (
+    <form>
+      <div>
+        <label>Name: </label>
+        <input type="text"></input>
+        <p className="error-text"></p>
+      </div>
+      <div>
+        <label>Username: </label>
+        <input type="text"></input>
+        <p className="error-text"></p>
+      </div>
+      <div>
+        <label>Email: </label>
+        <input type="text"></input>
+        <p className="error-text"></p>
+      </div>
+      <div>
+        <label>City: </label>
+        <input type="text"></input>
+        <p className="error-text"></p>
+      </div>
+      <div>
+        <label>Zipcode: </label>
+        <input type="text"></input>
+        <p className="error-text"></p>
+      </div>
+      <div>
+        <label>Phone: </label>
+        <input type="tel"></input>
+        <p className="error-text"></p>
+      </div>
+      <div>
+        <button>Save</button>
+      </div>
+    </form>
+  );
 };
 
 export default ClientForm;
