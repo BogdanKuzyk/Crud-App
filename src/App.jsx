682dd4314a1781ca829609fc324aed764662ebc9
Bogdan Kuzyk <bogdankuzyk14@gmail.com>
2023-12-14 11:34:19 +0200

    Fetch users and display them in ClientsDisplay component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 import ClientForm from "./components/ClientForm";
 import ClientsDisplay from "./components/ClientsDisplay";
 
+const usersInfo = [];
+
 function App() {
+  const [users, setUsers] = useState(usersInfo);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        const usersData = await response.json();
+        setUsers(usersData);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
   return (
     <div>
       <ClientForm />
-      <ClientsDisplay /> .
+      <ClientsDisplay users={users} /> .
     </div>
   );
 }
--- src/components/ClientsDisplay.jsx
+++ src/components/ClientsDisplay.jsx
@@ -1,5 +1,32 @@
-const ClientsDisplay = () => {
-  return <div></div>;
+const ClientsDisplay = ({ users }) => {
+  return (
+    <div>
+      {users.map((user) => (
+        <div key={user.id}>
+          <p>
+            <strong>Name:</strong> {user.name}
+          </p>
+          <p>
+            <strong>Username:</strong> {user.username}
+          </p>
+          <p>
+            <strong>Email:</strong> {user.email}
+          </p>
+          <p>
+            <strong>City:</strong> {user.address.city}
+          </p>
+          <p>
+            <strong>Zipcode:</strong> {user.address.zipcode}
+          </p>
+          <p>
+            <strong>Phone:</strong> {user.phone}
+          </p>
+
+          <hr />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ClientsDisplay;
