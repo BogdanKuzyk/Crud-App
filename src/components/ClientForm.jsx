a6b0d884a926074358464247ce5a3263510ecc7a
Bogdan Kuzyk <bogdankuzyk14@gmail.com>
2023-12-17 11:02:46 +0200

    Style Client Form

--- src/components/ClientForm.jsx
+++ src/components/ClientForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./ClientForm.css";
 
 const ClientForm = ({ addNewUser }) => {
   const [name, setName] = useState("");
@@ -134,45 +135,75 @@ const ClientForm = ({ addNewUser }) => {
       if (!/^\d+$|-+$/.test(zipcode)) {
         setZipcodeError("Zip code must contain only digits or a hyphen");
       }
-      if (!/^(\+?\d+)?$/.test(phone)) {
+      if (/^(\+?\d+)?$/.test(phone)) {
         setPhoneError("You must add a phone number");
       }
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
+    <form onSubmit={handleSubmit} className="form__controls">
+      <div className="form__controls__control">
         <label>Name: </label>
-        <input type="text" value={name} onChange={handleNameChange} />
+        <input
+          type="text"
+          value={name}
+          onChange={handleNameChange}
+          style={{ border: nameError ? "1px solid red" : "1px solid #ccc" }}
+        />
         <p className="error-text">{nameError}</p>
       </div>
-      <div>
+      <div className="form__controls__control">
         <label>Username: </label>
-        <input type="text" value={username} onChange={handleUsernameChange} />
+        <input
+          type="text"
+          value={username}
+          onChange={handleUsernameChange}
+          style={{ border: usernameError ? "1px solid red" : "1px solid #ccc" }}
+        />
         <p className="error-text">{usernameError}</p>
       </div>
-      <div>
+      <div className="form__controls__control">
         <label>Email: </label>
-        <input type="text" value={email} onChange={handleEmailChange} />
+        <input
+          type="text"
+          value={email}
+          onChange={handleEmailChange}
+          style={{ border: emailError ? "1px solid red" : "1px solid #ccc" }}
+        />
         <p className="error-text">{emailError}</p>
       </div>
-      <div>
+      <div className="form__controls__control">
         <label>City: </label>
-        <input type="text" value={city} onChange={handleCityChange} />
+        <input
+          type="text"
+          value={city}
+          onChange={handleCityChange}
+          style={{ border: cityError ? "1px solid red" : "1px solid #ccc" }}
+        />
         <p className="error-text">{cityError}</p>
       </div>
-      <div>
+      <div className="form__controls__control">
         <label>Zipcode: </label>
-        <input type="text" value={zipcode} onChange={handleZipcodeChange} />
+        <input
+          type="text"
+          value={zipcode}
+          onChange={handleZipcodeChange}
+          style={{ border: zipcodeError ? "1px solid red" : "1px solid #ccc" }}
+        />
         <p className="error-text">{zipcodeError}</p>
       </div>
-      <div>
+      <div className="form__controls__control">
         <label>Phone: </label>
-        <input type="tel" value={phone} onChange={handlePhoneChange} />
+        <input
+          type="tel"
+          value={phone}
+          onChange={handlePhoneChange}
+          style={{ border: phoneError ? "1px solid red" : "1px solid #ccc" }}
+        />
         <p className="error-text">{phoneError}</p>
       </div>
-      <div>
+      <div className="form__controls__control">
         <button type="submit">Save</button>
       </div>
     </form>
